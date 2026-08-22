f702ca1175dbff97ef30e67b50f3c9270c647e0a
SuriyaJohnson <sandanamsuriya@gmail.com>
2026-08-22 18:24:36 +0530

    Updated API URL

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,82 +1,61 @@
+// ================= BASE URL =================
+
+const BASE_URL = "https://employee-management-system-production-2d13.up.railway.app";
+
 // ================= SAVE =================
 
 function saveEmployee() {
 
     let employee = {
-
         id: document.getElementById("empId").value,
         name: document.getElementById("empName").value,
         salary: document.getElementById("salary").value
-
     };
 
-    fetch("http://localhost:8081/employee", {
-
+    fetch(`${BASE_URL}/employee`, {
         method: "POST",
-
         headers: {
             "Content-Type": "application/json"
         },
-
         body: JSON.stringify(employee)
-
     })
-
     .then(response => response.text())
-
     .then(message => {
-
         alert(message);
-
         clearForm();
-
         getAllEmployees();
-
     });
-
 }
-
 
 // ================= VIEW =================
 
 function getAllEmployees() {
 
-    fetch("http://localhost:8081/allemployee")
-
+    fetch(`${BASE_URL}/allemployee`)
     .then(response => response.json())
-
     .then(data => {
 
         let tableBody = document.getElementById("employeeTable");
-
         tableBody.innerHTML = "";
 
         data.forEach(employee => {
 
             tableBody.innerHTML += `
-
                 <tr>
-
                     <td>${employee.id}</td>
-
                     <td>${employee.name}</td>
-
                     <td>${employee.salary}</td>
-
                     <td>
                         <button onclick="editEmployee(${employee.id})">
                             Edit
                         </button>
                     </td>
-
                     <td>
                         <button onclick="deleteEmployee(${employee.id})">
                             Delete
                         </button>
                     </td>
-
                 </tr>
-
             `;
 
         });
@@ -85,15 +64,12 @@ function getAllEmployees() {
 
 }
 
-
 // ================= EDIT =================
 
 function editEmployee(id) {
 
-    fetch(`http://localhost:8081/employee/${id}`)
-
+    fetch(`${BASE_URL}/employee/${id}`)
     .then(response => response.json())
-
     .then(employee => {
 
         document.getElementById("empId").value = employee.id;
@@ -103,7 +79,6 @@ function editEmployee(id) {
     });
 
 }
-
 
 // ================= UPDATE =================
 
@@ -117,53 +92,42 @@ function updateEmployee() {
 
     };
 
-    fetch("http://localhost:8081/employee", {
+    fetch(`${BASE_URL}/employee`, {
 
         method: "PUT",
-
         headers: {
             "Content-Type": "application/json"
         },
-
         body: JSON.stringify(employee)
 
     })
 
     .then(response => response.text())
-
     .then(message => {
 
         alert(message);
-
         clearForm();
-
         getAllEmployees();
 
     });
 
 }
 
-
 // ================= DELETE =================
 
 function deleteEmployee(id) {
 
-    if(confirm("Are you sure you want to delete this employee?")){
+    if (confirm("Are you sure you want to delete this employee?")) {
 
-        fetch(`http://localhost:8081/delete/${id}`, {
-
+        fetch(`${BASE_URL}/delete/${id}`, {
             method: "DELETE"
-
         })
 
         .then(response => response.text())
-
         .then(message => {
 
             alert(message);
-
             clearForm();
-
             getAllEmployees();
 
         });
@@ -172,10 +136,9 @@ function deleteEmployee(id) {
 
 }
 
-
 // ================= CLEAR =================
 
-function clearForm(){
+function clearForm() {
 
     document.getElementById("empId").value = "";
     document.getElementById("empName").value = "";
